62883d73c8681631c63279c14f468c019d6ddd93
David Hybner <dah1998@yahoo.com>
2022-12-08 16:26:00 -0500

    added styling for app.js and Workspace.js

--- client/src/pages/Workspace.js
+++ client/src/pages/Workspace.js
@@ -1,27 +1,23 @@
 // Landing Page
 import React from 'react';
 
+import "../stylesheets/Workspace.css";
+import people from '../images/people.svg';
 
 function Workspace() {
-    return(
-        <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"></input>
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-    )
-    // hi
+  return (
+    <div >
+      <div class="container mt-5 header">
+        <h1 className='h1x'>Workspaces</h1>
+        <img className='sized' alt='working-people' src={people}></img>
+      </div>
+      <h2 className='h2x mt-5'>Your tracked Projects:</h2>
+      
+    </div>
+
+
+  )
+  // hi
 
 
 }
