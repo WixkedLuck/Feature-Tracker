d7d07ef39ca35adcbcf2cbff2cf24ae39b4105d1
Molly Salmonsen <salmonsen.molly@gmail.com>
2022-12-08 20:44:43 -0500

    Working on Models

---
+++ server/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+const Order = require('./Workspace');
+
+const userSchema = new Schema({
+    
+})
---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const Task = require('./Task');
+const Workspace = require('./Workspace');
+const Project = require('./Project');
+const User = require('./User')
+
+module.exports = { Task, Workspace, Project, User }
