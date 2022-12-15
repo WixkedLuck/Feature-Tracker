44168bc4f1b34efba58039026ed3f45f33c8adfe
David Hybner <dah1998@yahoo.com>
2022-12-15 18:02:23 -0500

    added modal/ styling for Inproject

--- client/src/pages/Inproject.js
+++ client/src/pages/Inproject.js
@@ -8,8 +8,10 @@
 
 // IMPORTS
 import React from "react";
+import { FaTrashAlt, FaHistory } from "react-icons/fa";
+
 import TaskList from '../components/TaskItem';
-// import Navbar from "../components/Nav";
+import "../stylesheets/Inproject.css"
 // import TaskList form "../components/TaskList";
 // import ProjectItem from "../components/ProjectItem"
 
@@ -24,20 +26,91 @@ import TaskList from '../components/TaskItem';
 
 // Select task & be re-directed to task itself 
 // 
+
 function Inproject() {
     return (
-        <div className="container text-center">
+        <div className="container text-center mt-5">
             <div className="row">
 
 
                 {/* Smaller Column */}
                 <div className="col-4">
-                    <div className="project-info">
+                    <div className="project-info bg-lar">
                         <h2>Project Name</h2>
-                        {/* <h4>Project Description blah blah stuff goes here</h4> */}
-                        <button href='#' className="btn btn-primary">all Projects</button>
-                        <button href='#' className="btn btn-primary">add to team</button>
-                        <button href='#' className="btn btn-primary">add task</button>
+                        <div className="sectionBreak">
+                            <h4>Team:</h4>
+                            <div class="container text-center">
+                                <div class="row">
+                                    <div class="col">
+                                        David
+                                    </div>
+                                    <div class="col">
+                                        Molly
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        Bryan
+                                    </div>
+                                    <div class="col">
+                                        Ely
+                                    </div>
+                                </div>
+
+                            </div>
+                            <button href='#' className="btn btn-primary btn-pd">Edit team</button><br></br>
+                            <button href='#' className="btn btn-primary btn-pd"> ← Go Back</button>
+                            {/* this line is for the model */}
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                Add Task
+                            </button>
+
+                            {/* <!-- Modal --> */}
+                            <div class="modal fade bg-dark " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog ">
+                                    <div class="modal-content styled mt-5">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5 ftcolor" id="exampleModalLabel">Create new Task:</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <form id="survey-form" className='formcolor'>
+
+                                                <label for="name" id="name-label">Task Description:</label>
+                                                <textarea name="message" required placeholder='\\TODO' /><br></br>
+
+                                                <label for="description" id="name-label">Task Priority:</label>
+                                                <select value="newBugPriority">
+                                                    <option value="Low">Low</option>
+                                                    <option value="Medium">Medium</option>
+                                                    <option value="High">High</option>
+                                                </select><br></br>
+
+
+                                                <button type="submit" class="btn btn-primary" id="submit">Submit</button>
+
+                                            </form>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* end modal */}
+
+
+
+
+
+
+
+
+                        </div>
+
+
                     </div>
                 </div>
 
@@ -45,25 +118,38 @@ function Inproject() {
                 {/* Table of Tasks */}
                 <div className="container col-8">
                     {/* header info - static info */}
-                    <div className="row">
-                        <div className="col-sm">
-                            Title
-                        </div>
-                        <div className="col-sm">
-                            Priority
-                        </div>
-                        <div className="col-sm">
-                            Status
-                        </div>
-                        <div className="col-sm">
-                            Update
-                        </div>
-                    </div>
+
+                    <table class="table table-dark table-striped trial">
+                        <thead>
+                            <tr>
+
+                                <th scope="col">Task Description</th>
+                                <th scope="col">Priority</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Update</th>
+                                <th scope="col">Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>This is a Description of the problem This is a Description of the problem</td>
+                                <td>High</td>
+                                <td>Completed</td>
+                                <td><button className="btn btn-primary btn-pd"><FaHistory /></button></td>
+                                <td><button className="btn btn-primary btn-pd"><FaTrashAlt /></button></td>
+                            </tr>
+                           
+                        </tbody>
+                    </table>
                     {/* items mapped over - responsive info */}
                     {/* grab all tasks from user and create list */}
-                    {<TaskList />} 
+                    {/* {<TaskList />}  */}
                 </div>
             </div>
+
+
+
+
         </div>
     )
 }
