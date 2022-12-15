ce78ca07acd2ee8048a800c63f3a6a8fa367aba0
David Hybner <dah1998@yahoo.com>
2022-12-15 15:27:08 -0500

    create handler for signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../stylesheets/signup.css"
-// Importing React thereabove, and generating/exporting a signup page.
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
 
-export default function Signup() {
+// Importing React thereabove, and generating/exporting a signup page.
+function Signup(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        firstName: formState.firstName,
+        lastName: formState.lastName,
+      },
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+
     return(
     <div className="col-md-6 mx-auto mt-4">
       
     
-        <form className="form signup-form ">
+        <form className="form signup-form " onSubmit={handleFormSubmit} >
           <div className="form-group">
           <h2>Signup</h2>
             <label for="name-signup" id="name-signup-text">First Name:</label>
-            <input className="form-input " required placeholder='John' type={"text"} id="fname-signup" />
+            <input className="form-input " required placeholder='John' name="firstName"  type="firstName" id="fname-signup" onChange={handleChange} />
           </div>
           <div className="form-group mt-1">
           <label for="name-signup" id="name-signup-text">Last Name:</label>
-            <input className="form-input " required  placeholder=' Doe' type={"text"} id="Lname-signup" />
+            <input className="form-input " required  placeholder='Doe' name='lastName' type="lastName" onChange={handleChange} id="Lname-signup" />
           </div>
           <div className="form-group mt-1 ">
             <label  for="email-signup" id="email-signup-text" >Email:</label>
-            <input className="form-input " required placeholder='Email@gmail.com' type={"text"} id="email-signup" />
+            <input className="form-input " required placeholder='Email@gmail.com' name='email' type="email" onChange={handleChange}id="email-signup" />
           </div>
           <div className="form-group mt-1">
             <label for="password-signup" id="password-signup-text" >Password:</label>
-            <input className="form-input" required placeholder='********' type={"password"} id="password-signup" />
+            <input className="form-input" required placeholder='********'  name='password' type="password" onChange={handleChange} id="password-signup" />
           </div>
           <div className="form-group mt-1">
            
@@ -34,4 +63,6 @@ export default function Signup() {
     
   
     )
+
 }
+export default Signup;
