0303f4672616892db61b63b3d777cf91d2997b9f
David Hybner <dah1998@yahoo.com>
2022-12-15 15:41:21 -0500

    login form takes in valid users and rejects non

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,30 +1,65 @@
-import React from 'react';
-import "../stylesheets/Login.css"
-// Importing React. Creating the layout for the login screen.
+import React, { useState } from 'react';
+import "../stylesheets/Login.css";
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
 
-export default function Login() {
+ function Login(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
     return(
         <div className="col-md-6 mx-auto mt-4">
    
 
-    <form className="form login-form">
+    <form className="form login-form" onSubmit={handleFormSubmit}>
     <h2>Login</h2>
       <div className="form-group">
 
         <label for="email-login" id="email-login-text" >Email:</label>
-        <input className="form-input" id="email-login" required  placeholder='SomeEmail@gmail.com' type={"text"} />
+        <input className="form-input" id="email-login" required  placeholder='SomeEmail@gmail.com' name="email" type="email" onChange={handleChange} />
       </div>
       <div className="form-group mt-1">
         <label for="password-login" id="password-login-text" >Password:</label>
 
-        <input className="form-input" id="password-login" required placeholder='******' type={"password"} />
+        <input className="form-input" id="password-login" required placeholder='******' name="password" type="password"  onChange={handleChange}/>
       </div>
       <div className="form-group d-flex">
+     
      
         <button className="btn btn-primary center mt-3" type={"submit"}>login</button>
       </div>
       <a className='mt-5' href="/signup">Sign-up</a>
+      {error ? (
+            <div>
+            <p className="error-text"> credentials are incorrect</p>
+            <p className="error-text"> try again</p>
+            </div>
+        ) : null}
     </form>
 </div>
     )
 }
+
+export default Login;
